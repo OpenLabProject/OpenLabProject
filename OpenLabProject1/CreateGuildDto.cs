f6de8e959eddc0b7047401d6ecf5317cf5c2a5c7
Michal Bernát <michal.bernat.l312@gmail.com>
2023-12-19 10:04:24 +0100

    reactive form, create guild dto

---
+++ OpenLabProject1/CreateGuildDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenLabProject1
+{
+    public class CreateGuildDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public int GuildMaxMembers { get; set; }
+    }
+}
